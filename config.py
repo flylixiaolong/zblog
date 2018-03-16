bbeafd9ebd38e4badb86115323bbae6ea434a82b
lixiaolong <lixl@diandainfo.com>
2018-03-16 16:52:04 +0800

    remove index.html in root

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ class Config(object):
     """Base config"""
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:123456@localhost/blog"
