0e5b005c2c8eea168d3994b2ae663c4a759b0853
lixiaolong <lixl@diandainfo.com>
2018-03-22 09:37:17 +0800

    login success

--- blog/admin/views.py
+++ blog/admin/views.py
@@ -1,9 +1,20 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_user
+from blog.models import Admin
+from blog import db
 
 admin = Blueprint('admin', __name__, url_prefix='/admin', template_folder='templates')
 
 
 @admin.route('/login', methods=['GET', 'POST'])
 def login():
+    if(request.method=='POST'):
+        user_name = request.form['user_name']
+        password = request.form['password']
+        user = db.session.query(Admin).filter(Admin.user_name==user_name).one_or_none()
+        if(user and user.verify_password(password)):
+            login_user(user)
+            return '<p> hello you! <p>'
+        flash('Invalid username or password.')
     return render_template('admin/login.html')
+        
--- config.py
+++ config.py
@@ -3,9 +3,14 @@
 """
 
 
+import os
+
+
 class Config(object):
     """Base config"""
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:123456@localhost/blog"
+    SECRET_KEY = os.environ.get(
+        'SECRET_KEY') or b'\x14\xf0I#\xde\x1fze*P\xc0\xa3|\x10\xf4\xa4\x05\x9b\xc7}\xa5\x0f\x12K'
